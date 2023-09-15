89ad78a1523713c0a956a674f9631d2f6cdd8416
luis Colina <colina.1990lz@gmail.com>
2023-09-15 08:31:39 -0400

    step component created

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
   ];
   return (
     <>
-      <StepController />
+      <StepController goToStep={goToStep}/>
       <FormContainer>{formsArray[step]}</FormContainer>
     </>
   );
--- src/components/Organisms/StepController.tsx
+++ src/components/Organisms/StepController.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
-const StepController = () => {
+type Props = {
+  goToStep: (number: number) => void
+}
+const StepController = ({goToStep}: Props) => {
+  const steps = [1,2,3,4]
+  //ask chatgpt why is go to step not a function
   return (
-    <div>StepController</div>
+    <div>
+      {steps.map((e,i) => <button onClick={() => goToStep(i)}>{e}</button>)}
+    </div>
   )
 }
 
