d0c57589ec94e6a57ac382ab35406fdbf32bdb0b
luis Colina <colina.1990lz@gmail.com>
2023-10-03 09:15:08 -0400

    success stage added and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,57 @@
-import { useState } from 'react'
-import StepController from './components/Organisms/StepController'
-import { useFormSession} from './hooks/useFormSession'
-import FormContainer from './components/Organisms/FormContainer'
-import Form1 from './components/Molecules/Form1'
-import Form2 from './components/Molecules/Form2'
-import Form3 from './components/Molecules/Form3'
-import Form4 from './components/Molecules/Form4'
-import BtnNavigation from './components/Atoms/BtnNavigation'
-
+import { useState } from "react";
+import StepController from "./components/Organisms/StepController";
+import { useFormSession } from "./hooks/useFormSession";
+import FormContainer from "./components/Organisms/FormContainer";
+import Form1 from "./components/Molecules/Form1";
+import Form2 from "./components/Molecules/Form2";
+import Form3 from "./components/Molecules/Form3";
+import Form4 from "./components/Molecules/Form4";
+import BtnNavigation from "./components/Atoms/BtnNavigation";
 
 function App() {
-  const {
-    order,
-    register,
-    errors,
-    step,
-    goToStep,
-  } = useFormSession();
-
-  const [isYearly, setOnYearly] = useState<boolean>(order.yearlyBillingCycle)
-  const OnYearly = () => setOnYearly((prev) => (!prev))
+  const { order, register, errors, step, goToStep } = useFormSession();
+  const [success, setSuccess] = useState<boolean>(false)
+  const handleSuccess = () => {
+    if(errors.name || errors.email || errors.phoneNumber || errors.selectedPlan){
+      return
+    }
+    setSuccess(true)
+  }
+  const [isYearly, setOnYearly] = useState<boolean>(order.yearlyBillingCycle);
+  const OnYearly = () => setOnYearly((prev) => !prev);
   const formsArray = [
-    <Form1 order={order} register={register} errors={errors} step={step}/>,
-    <Form2 order={order} register={register} errors={errors} isYearly={isYearly} OnYearly={OnYearly} step={step}/>,
-    <Form3 order={order} register={register} errors={errors} isYearly={isYearly} step={step}/>,
-    <Form4 order={order} errors={errors} step={step} goToStep={goToStep}/>
+    <Form1 order={order} register={register} errors={errors} step={step} />,
+    <Form2
+      order={order}
+      register={register}
+      errors={errors}
+      isYearly={isYearly}
+      OnYearly={OnYearly}
+      step={step}
+    />,
+    <Form3
+      order={order}
+      register={register}
+      errors={errors}
+      isYearly={isYearly}
+      step={step}
+    />,
+    <Form4 order={order} errors={errors} step={step} goToStep={goToStep} />,
   ];
 
   return (
     <>
       <StepController step={step} goToStep={goToStep} />
-      <FormContainer step={step}>
+      <FormContainer step={step} success={success}>
         {formsArray[0]}
         {formsArray[1]}
         {formsArray[2]}
         {formsArray[3]}
-        <BtnNavigation goToStep={goToStep} step={step} screen="desktop"/>
-        </FormContainer>
-        <BtnNavigation goToStep={goToStep} step={step} screen="mobile"/>
+        <BtnNavigation goToStep={goToStep} success={success} step={step} handleSuccess={handleSuccess} screen="desktop" />
+      </FormContainer>
+      <BtnNavigation goToStep={goToStep} success={success} step={step} handleSuccess={handleSuccess} screen="mobile" />
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Organisms/Success.tsx
+++ src/components/Organisms/Success.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import sucessImg from "../../assets/icon-thank-you.svg"
+import styles from "../styles/success.module.css"
 const Success = () => {
   return (
-    <div>Success</div>
+    <div className={styles.container}>
+
+        <figure>
+            <img src={sucessImg} alt="thank you" />
+        </figure>
+        <h2>Thank you!</h2>
+        <p>Thanks for confirming your subscription! We hope you have fun 
+  using our platform. If you ever need support, please feel free 
+  to email us at support@loremgaming.com.</p>
+
+    </div>
   )
 }
 
